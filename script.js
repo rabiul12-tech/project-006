c252242a3e47caff886bd0feb5271d456040a10a
rabiul12-tech <rabiulalamin@gmail.com>
2025-10-11 19:04:03 +0600

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+// Project 6 minimal JS
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("actionBtn");
+  if (btn) {
+    btn.addEventListener("click", () => {
+      alert("Hello from Project 6! 🎉");
+    });
+  }
+});
